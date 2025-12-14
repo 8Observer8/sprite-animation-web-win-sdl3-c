adbaabd598b871a92751baf04d86ffae19cf1d58
Ivan Enzhaev <8observer8@gmail.com>
2025-12-14 21:36:58 +0400

    Change paths

--- config-web.bat
+++ config-web.bat
@@ -1,4 +1,4 @@
-emcmake cmake -G "MinGW Makefiles" -S . -B dist/web
--DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/web/lib/cmake/SDL3
--DSDL3_image_DIR=E:/libs/sdl3_image-3.1.0-prefix/web/lib/cmake/SDL3_image
+emcmake cmake -S . -B dist/web ^
+-DSDL3_DIR=H:/libs/sdl-3.1.6-prefix/web/lib/cmake/SDL3 ^
+-DSDL3_image_DIR=H:/libs/sdl3_image-3.1.0-prefix/web/lib/cmake/SDL3_image ^
 -DCMAKE_BUILD_TYPE=Debug
--- config-win.bat
+++ config-win.bat
@@ -1,4 +1,4 @@
-cmake -G "MinGW Makefiles" -S . -B dist/win
--DSDL3_DIR=E:/libs/sdl-3.1.6-prefix/win/lib/cmake/SDL3
--DSDL3_image_DIR=E:/libs/sdl3_image-3.1.0-prefix/win/lib/cmake/SDL3_image
+cmake -G "MinGW Makefiles" -S . -B dist/win ^
+-DSDL3_DIR=H:/libs/sdl-3.1.6-prefix/win/lib/cmake/SDL3 ^
+-DSDL3_image_DIR=H:/libs/sdl3_image-3.1.0-prefix/win/lib/cmake/SDL3_image ^
 -DCMAKE_BUILD_TYPE=Debug
