805e11bb822b4bccace5427db82bffc8d794f0d1
Ivan Enzhaev <8observer8@gmail.com>
2025-12-14 21:32:36 +0400

    Clean up resources

--- src/main.c
+++ src/main.c
@@ -15,22 +15,23 @@ const float animationPeriod = 0.1f;
 
 // Define animation frames (adjust dimensions and positions as needed)
 SDL_FRect frames[4] = {
-    { 0, 0, 128, 128 }, // Frame 1
-    { 128, 0, 128, 128 }, // Frame 2
-    { 0, 128, 128, 128 }, // Frame 3
-    { 128, 128, 128, 128 }  // Frame 4
+    { 0, 0, 128, 128 },    // Frame 1
+    { 128, 0, 128, 128 },  // Frame 2
+    { 0, 128, 128, 128 },  // Frame 3
+    { 128, 128, 128, 128 } // Frame 4
 };
 
 // This function runs once at startup
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
     if (!SDL_CreateWindowAndRenderer("Sprite animation using SDL3 and C",
-        350, 350, 0, &window, &renderer)) //
+            350, 350, 0, &window, &renderer)) //
     {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
@@ -40,7 +41,8 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 
     const char *texturePath = "./assets/sprites/spritesheet.png";
     texture = IMG_LoadTexture(renderer, texturePath);
-    if (!texture) {
+    if (!texture)
+    {
         SDL_Log("Couldn't load %s: %s\n", texturePath, SDL_GetError());
         return SDL_APP_FAILURE;
     }
@@ -51,17 +53,20 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 // This function runs when a new event (mouse input, keypresses, etc) occurs
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_QUIT) {
+    if (event->type == SDL_EVENT_QUIT)
+    {
         return SDL_APP_SUCCESS; // End the program, reporting success to the OS
     }
-    return SDL_APP_CONTINUE; // Carry on with the program!
+    return SDL_APP_CONTINUE;
 }
 
 // This function runs once per frame, and is the heart of the program
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
-    SDL_SetRenderDrawColor(renderer, 50, 50, 50, SDL_ALPHA_OPAQUE); // Canvas color
-    SDL_RenderClear(renderer); // Clear the canvas and fill it with the canvas color
+    // Canvas color
+    SDL_SetRenderDrawColor(renderer, 50, 50, 50, SDL_ALPHA_OPAQUE);
+    // Clear the canvas and fill it with the canvas color
+    SDL_RenderClear(renderer);
 
     // Update and render animation
     SDL_FRect srcRect = frames[frameIndex];
@@ -81,7 +86,8 @@ SDL_AppResult SDL_AppIterate(void *appstate)
     animationTime += deltaTime;
 
     // Increment frame index and loop back to 0 when necessary
-    if (animationTime > animationPeriod) {
+    if (animationTime > animationPeriod)
+    {
         frameIndex = (frameIndex + 1) % 4;
         animationTime = 0.f;
     }
@@ -92,5 +98,23 @@ SDL_AppResult SDL_AppIterate(void *appstate)
 // This function runs once at shutdown
 void SDL_AppQuit(void *appstate, SDL_AppResult result)
 {
-    // SDL will clean up the window/renderer for us
+    if (texture)
+    {
+        SDL_DestroyTexture(texture);
+        texture = NULL;
+    }
+
+    if (renderer)
+    {
+        SDL_DestroyRenderer(renderer);
+        renderer = NULL;
+    }
+
+    if (window)
+    {
+        SDL_DestroyWindow(window);
+        window = NULL;
+    }
+
+    SDL_Quit();
 }
